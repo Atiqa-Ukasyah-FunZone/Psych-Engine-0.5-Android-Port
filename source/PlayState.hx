df0a03c2da7c52af6218debd0b8a1fbca7c3ffc2
Atiqa-Ukasyah-FunZone <99643716+Atiqa-Ukasyah-FunZone@users.noreply.github.com>
2022-02-16 14:17:32 +0800

    Update again the rating
    I just want lol

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -69,15 +69,15 @@ class PlayState extends MusicBeatState
 
 	public static var ratingStuff:Array<Dynamic> = [
 		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Suck!', 0.4], //From 20% to 39%
+		['Meh', 0.5], //From 40% to 49%
+		['BAD', 0.6], //From 50% to 59%
+		['Good enough', 0.69], //From 60% to 68%
+		['Bloody', 0.7], //69%
+		['GREAT', 0.8], //From 70% to 79%
+		['Evil', 0.9], //From 80% to 89%
+		['Monter', 1], //From 90% to 99%
+		['OVERPOWER!!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 	
 	public var modchartTweens:Map<String, FlxTween> = new Map<String, FlxTween>();
